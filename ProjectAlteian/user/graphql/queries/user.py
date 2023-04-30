bfad3b2235eb90e3558706ae22746c472d0fb176
Alteian <davosroucka@gmail.com>
2023-04-30 06:18:22 +0200

    list and default filtering

--- ProjectAlteian/user/graphql/queries/user.py
+++ ProjectAlteian/user/graphql/queries/user.py
@@ -1,9 +1,31 @@
 import typing
+
+from strawberry.types import Info
 from strawberry_django_plus import gql
+from strawberry_django_plus.optimizer import optimize
+from strawberry_django.filters import apply as apply_filters
+from strawberry_django.ordering import apply as apply_ordering
 
 from ProjectAlteian.user.graphql.types import UserType
+from ProjectAlteian.user.graphql.filters import UserFilter
+from ProjectAlteian.user.graphql.orders import UserOrder
+from ProjectAlteian.user.models import User
 
 @gql.type
 class UserQuery:
     user: typing.Optional[UserType] = gql.django.node()
     user_connections: gql.relay.Connection[UserType] = gql.django.connection()
+    
+    @gql.django.field()
+    def user_list(
+        self,
+        info: Info,
+        filters: typing.Optional[UserFilter] = None,
+        order: typing.Optional[UserOrder] = None,
+        ) -> typing.List[UserType]:
+        queryset = optimize(UserType.get_queryset(User.objects.all(), info), info)
+        if filters:
+            queryset = apply_filters(queryset, filters)
+        if order:
+            queryset = apply_ordering(queryset, order)
+        return queryset
--- ProjectAlteian/user/graphql/types/user.py
+++ ProjectAlteian/user/graphql/types/user.py
@@ -1,6 +1,7 @@
-import uuid
-
 from strawberry_django_plus import gql
+from strawberry.types import Info
+
+from django.db.models.query import QuerySet
 
 from ProjectAlteian.user.models import User
 from ProjectAlteian.shared.utils import inject_all_fields
@@ -11,9 +12,20 @@ from ProjectAlteian.user.graphql.orders import UserOrder
 @gql.django.type(
     User, 
     filters=UserFilter, 
-    order=UserOrder, 
+    order=UserOrder,
+    pagination=True,
     )
 @inject_all_fields(User, ignore_fields=["password", "id"])
 class UserType(gql.relay.Node):
     id: gql.relay.GlobalID
     ...
+
+    @classmethod
+    def get_queryset(
+        cls,
+        queryset: QuerySet[User],
+        info: Info,
+        **kwargs,
+        ) -> QuerySet[User]:
+        queryset.filter(is_active=True)
+        return queryset
